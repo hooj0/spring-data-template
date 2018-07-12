6b4d794b1559102e74895a00cd9aa7044666c1b3
hoojo <hoojo@qq.com>
2018-07-12 15:38:41 +0800

    :sparkles: :white_check_mark: :construction: Added `uuid template repo` testunit

--- spring-data-template/src/test/java/io/github/hooj0/springdata/template/repository/support/UUIDTemplateRepositoryTest.java
+++ spring-data-template/src/test/java/io/github/hooj0/springdata/template/repository/support/UUIDTemplateRepositoryTest.java
@@ -1,11 +1,21 @@
 package io.github.hooj0.springdata.template.repository.support;
 
+import java.util.UUID;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import io.github.hooj0.springdata.template.config.AbstractTemplateConfiguration;
+import io.github.hooj0.springdata.template.core.MyTplTemplate;
+import io.github.hooj0.springdata.template.repository.TemplateRepository;
+import io.github.hooj0.springdata.template.repository.config.EnableTemplateRepositories;
 import io.github.hooj0.springdata.template.repository.domain.User;
 
 /**
@@ -20,11 +30,23 @@ import io.github.hooj0.springdata.template.repository.domain.User;
  * @version 1.0
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:/simple-repository-test.xml")
+@ContextConfiguration
 public class UUIDTemplateRepositoryTest {
 
+	@Configuration
+	@EnableTemplateRepositories(basePackageClasses = UUIDTemplateRepo.class,
+	considerNestedRepositories = true, 
+	includeFilters = @Filter(pattern = ".*UUIDTemplateRepo", type = FilterType.REGEX))
+	public static class Config extends AbstractTemplateConfiguration {
+		
+		@Bean
+		MyTplTemplate myTplTemplate() throws ClassNotFoundException {
+			return new MyTplTemplate("UUIDTemplateRepositoryTest", this.templateConverter());
+		}
+	}
+	
 	@Autowired
-	private UUIDTemplateRepository<User> repo;
+	private UUIDTemplateRepo repo;
 	
 	@Test
 	public void testCRUD() {
@@ -32,5 +54,9 @@ public class UUIDTemplateRepositoryTest {
 		repo.refresh();
 		repo.query("Xxxxx");
 		repo.invoke("....");
+	}
+	
+	interface UUIDTemplateRepo extends TemplateRepository<User, UUID> {
+		
 	}
 }
