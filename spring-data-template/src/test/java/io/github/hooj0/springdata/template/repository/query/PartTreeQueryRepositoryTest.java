98ba17ff0b21830b0ea0f035e27e35532f65a1c7
hoojo <hoojo@qq.com>
2018-07-16 10:13:47 +0800

    :sparkles: :bento:  :white_check_mark: Added `part tree query repo` testunit

--- spring-data-template/src/test/java/io/github/hooj0/springdata/template/repository/query/PartTreeQueryRepositoryTest.java
+++ spring-data-template/src/test/java/io/github/hooj0/springdata/template/repository/query/PartTreeQueryRepositoryTest.java
@@ -1,10 +1,25 @@
 package io.github.hooj0.springdata.template.repository.query;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import io.github.hooj0.springdata.template.annotations.CountQuery;
 import io.github.hooj0.springdata.template.annotations.Query;
+import io.github.hooj0.springdata.template.config.AbstractTemplateConfiguration;
+import io.github.hooj0.springdata.template.core.MyTplTemplate;
 import io.github.hooj0.springdata.template.repository.TemplateRepository;
+import io.github.hooj0.springdata.template.repository.config.EnableTemplateRepositories;
 import io.github.hooj0.springdata.template.repository.query.TemplatePartQueryTest.AddressType;
 import io.github.hooj0.springdata.template.repository.query.TemplatePartQueryTest.AllowFiltering;
 import io.github.hooj0.springdata.template.repository.query.TemplatePartQueryTest.Person;
@@ -12,7 +27,7 @@ import io.github.hooj0.springdata.template.repository.query.TemplatePartQueryTes
 
 
 /**
- * <b>function:</b>
+ * <b>function:</b> PartTreeQueryRepositoryTest
  * @author hoojo
  * @createDate 2018年7月13日 下午7:23:23
  * @file PartTreeQueryRepositoryTest.java
@@ -22,11 +37,38 @@ import io.github.hooj0.springdata.template.repository.query.TemplatePartQueryTes
  * @email hoojo_@126.com
  * @version 1.0
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
 public class PartTreeQueryRepositoryTest {
 
-	interface Repo extends TemplateRepository<Person, String> {
+	@Configuration
+	@EnableTemplateRepositories(basePackageClasses = MyRepo.class,
+		considerNestedRepositories = true, 
+		includeFilters = @Filter(pattern = ".*MyRepo", type = FilterType.REGEX))
+	public static class Config extends AbstractTemplateConfiguration {
+		
+		@Override
+		protected Set<Class<?>> getInitialEntitySet() {
+			return Collections.singleton(Person.class);
+		}
+		
+		@Bean
+		MyTplTemplate myTplTemplate() throws ClassNotFoundException {
+			return new MyTplTemplate("PartTreeQueryRepositoryTest", this.templateConverter());
+		}
+	}
+	
+	@Autowired
+	private MyRepo repo;
+	
+	@Test
+	public void testFindByFirstname() {
+		System.out.println(repo.findByFirstname("jack"));
+	}
+	
+	interface MyRepo extends TemplateRepository<Person, String> {
 
-		@Query
+		@Query("select -> lastname = ?0")
 		Person findByLastname(String lastname);
 
 		Person findTop3By();
@@ -35,7 +77,7 @@ public class PartTreeQueryRepositoryTest {
 
 		Person findPersonByFirstnameAndLastname(String firstname, String lastname);
 
-		Person findByFirstnameOrLastName(String firstname);
+		Person findByFirstnameOrLastname(String lastname);
 
 		@CountQuery
 		Person findPersonBy();
